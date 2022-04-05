5346a5b59c6167d258d8d4bc942d8500fecce254
Cyprian Zdebski <57770826+shipurjan@users.noreply.github.com>
2022-04-05 18:34:04 +0200

    tmp

--- src/UnlitCube.java
+++ src/UnlitCube.java
@@ -51,45 +51,46 @@ public class UnlitCube extends GLJPanel implements GLEventListener, KeyListener 
     double rotateY = -15;
     double rotateZ = 0;
     
-    private void square(GL2 gl2, double r, double g, double b) {
+    private void polygon(GL2 gl2, double r, double g, double b, int n) {
+    	//do zmiany funkcja
         gl2.glColor3d(r,g,b);
         gl2.glBegin(GL2.GL_TRIANGLE_FAN);
-        gl2.glVertex3d(-0.5, -0.5, 0.5);
-        gl2.glVertex3d(0.5, -0.5, 0.5);
-        gl2.glVertex3d(0.5, 0.5, 0.5);
-        gl2.glVertex3d(-0.5, 0.5, 0.5);
+        gl2.glVertex3f(0.0f, 0.0f, 0.0f);
+        for(int i=0; i < n; i++) {
+        	gl2.glVertex3f(i*0.1f, i*0.1f, 0.0f); 
+        }
         gl2.glEnd();
     }
     
     private void cube(GL2 gl2, double size) {
         gl2.glPushMatrix();
         gl2.glScaled(size,size,size); // scale unit cube to desired size
-        
-        square(gl2,1, 0, 0); // red front face
+        final int n = 11;
+        polygon(gl2,1, 0, 0, n); // red front face
         
         gl2.glPushMatrix();
         gl2.glRotated(90, 0, 1, 0);
-        square(gl2,0, 1, 0); // green right face
+        polygon(gl2,0, 1, 0, n); // green right face
         gl2.glPopMatrix();
         
         gl2.glPushMatrix();
         gl2.glRotated(-90, 1, 0, 0);
-        square(gl2,0, 0, 1); // blue top face
+        polygon(gl2,0, 0, 1, n); // blue top face
         gl2.glPopMatrix();
         
         gl2.glPushMatrix();
         gl2.glRotated(180, 0, 1, 0);
-        square(gl2,0, 1, 1); // cyan back face
+        polygon(gl2,0, 1, 1, n); // cyan back face
         gl2.glPopMatrix();
         
         gl2.glPushMatrix();
         gl2.glRotated(-90, 0, 1, 0);
-        square(gl2,1, 0, 1); // magenta left face
+        polygon(gl2,1, 0, 1, n); // magenta left face
         gl2.glPopMatrix();
         
         gl2.glPushMatrix();
         gl2.glRotated(90, 1, 0, 0);
-        square(gl2,1, 1, 0); // yellow bottom face
+        polygon(gl2,1, 1, 0, n); // yellow bottom face
         gl2.glPopMatrix();
         
         gl2.glPopMatrix(); // Restore matrix to its state before cube() was called.
